e4d9cabb3701ae26fe0a573459567f15ce4203c9
Franco Larrosa <francolarrosa02@gmail.com>
2025-10-21 16:36:01 -0300

    Agregue el chequeo de sintaxis de la lista para el script 'ScriptBash/crearUsers.sh'

--- ScriptBash/crearUsers.sh
+++ ScriptBash/crearUsers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#El script recorre una lista pasada como primer parametro y crea los usuarios correspondientes
 
 #Defino opciones por defecto para campos vacios
 opdefcom="Comentario por defecto"
@@ -8,6 +7,7 @@ opdefhome="/home/$user"
 opdefcrearhome=True
 opdefshell="/bin/bash"
 
+#Se chequea que los parametros sean minimo 1 y maximo 3
 if [ $# -lt 1 ]; then
 	echo "Debe usar minimo 1 parametro (el archivo con la lista de usuarios)"
 	exit 0
@@ -18,6 +18,21 @@ if [ $# -gt 3 ]; then
 	exit 1
 fi
 
-if [ $(wc -c < $1) -lt 1 ] ; then
-	echo "El archivo/lista pasado como parametro {$1} esta vacio"
+#arch es el primer parametro correspondiente al archivo lista
+#regex es una variable que contiene la expresion regular a utilizar
+arch=$1
+regex='^[^:]*:[^:]*:[^:]*:(si|no):/bin/(bash|sh|zsh)$'
+
+#Se compara cantidad de lineas con la cantidad de lineas validas
+#Las lineas validas son aquellas que coincidan con la expresion regular
+totlineas=$(cat $arch | wc -l)
+validas=$(grep -Ec $regex $arch)
+novalidas=$(grep -Env $regex $arch)
+
+if [ $totlineas -eq $validas ]; then
+  echo "Todo correcto"
+else
+  echo "Errores detectados: $(($totlineas - $validas))"
+  echo -e "Lineas invalidas: \n$novalidas"
 fi
+
