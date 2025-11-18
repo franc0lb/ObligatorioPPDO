58323edf7f989396f0a35e97f2470aad4840defc
Franco Larrosa <francolarrosa02@gmail.com>
2025-11-18 09:43:28 -0300

    Se corrigen cosas del script de aws, queda pronto la parte del bucket, ec2, mover y descargar archivos y paquetes

--- ScriptAWS/ScriptAWS.py
+++ ScriptAWS/ScriptAWS.py
@@ -1,78 +1,78 @@
+# Importamos librerías necesarias que se utilizaran para las tareas que realiza el script
 import boto3
+import os
 from botocore.exceptions import ClientError
+import time
+
+######################################################################################################################################
+# 1: CREACION DEL BUCKET Y SUBIDA DE ARCHIVOS AL MISMO
+######################################################################################################################################
 
 s3 = boto3.client('s3')
 
+# Variables: Nombre del bucket, carpeta local donde estarán alojados los archivos bajados del repo, carpeta remota del s3 donde se van a subir
 bucket_name = 'app-bucket-obligatorio-241688'
-carpeta = '/home/franco/APP'
+carpeta_local = '/home/franco/APP/' #Yo alojé acá los archivos en mi VM, ajustar esta variable al directorio que corresponda
+carpeta_remota = 'APP'
 
-# Carpeta que se creará dentro del bucket para alojar archivos
-carpeta_remota = "APP"
+# Obtener todos los archivos de la carpeta local
+# os.listdir devuelve una lista con los nombres de los archivos y carpetas dentro de un directorio, solo nombres, no devuelve path completo
+archivos = os.listdir(carpeta_local)
 
-# Archivos locales que se van a subir
-archivos = {
-    f"{carpeta}/index.html": "index.html",
-    f"{carpeta}/login.php": "login.php",
-    f"{carpeta}/app.css": "app.css",
-    f"{carpeta}/app.js": "app.js",
-    f"{carpeta}/config.php": "config.php",
-    f"{carpeta}/index.php": "index.php",
-    f"{carpeta}/login.css": "login.css",
-    f"{carpeta}/login.html": "login.html",
-    f"{carpeta}/login.js": "login.js",
-    f"{carpeta}/init_db.sql": "init_db.sql",
-}
-
-# Crear bucket (si no existe)
+# Crear bucket si no existe
 try:
     s3.create_bucket(Bucket=bucket_name)
-    print(f"Bucket creado: {bucket_name}")
+    print("")
+    print(f"Creando Bucket: {bucket_name}")
 except ClientError as e:
-    if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
-        print(f"El bucket {bucket_name} ya existe y es tuyo.")
-    else:
+    if e.response['Error']['Code'] != 'BucketAlreadyOwnedByYou':
         print(f"Error creando bucket: {e}")
         exit(1)
-
-print(f"Subiendo archivos a S3 carpeta: {carpeta_remota}/")
-
-# Se suben los archivos al bucket dentro de /APP/
-for local_path, file_name in archivos.items():
-
-    # Key final: APP/archivo.extension
-    s3_key = f"{carpeta_remota}/{file_name}"
+print("")
+print(f"Subiendo archivos a s3://{bucket_name}/{carpeta_remota}/")
+print("")
+for nombre in archivos:
+    arch_local = os.path.join(carpeta_local, nombre) # os.path.join construye rutas de archivos o directorios, de forma segura (evita errores con barras "/"), en este caso arma el path con las 2 variables
+    s3_key = f"{carpeta_remota}/{nombre}" # esta variable contiene la ruta remota para los archivos en el bucket, f me permite insertar variables dentro del texto
 
     try:
-        s3.upload_file(local_path, bucket_name, s3_key)
-        print(f"Subido: {local_path}  →  s3://{bucket_name}/{s3_key}")
+        s3.upload_file(arch_local, bucket_name, s3_key)
+        print(f"Subido: {arch_local} → s3://{bucket_name}/{s3_key}")
     except FileNotFoundError:
-        print(f"ERROR: No existe el archivo local: {local_path}")
-    except ClientError as e:
-        print(f"ERROR subiendo {local_path}: {e}")
+        print(f"El archivo {arch_local} no existe")
 
-print("\nTodos los archivos fueron subidos correctamente.")
-print(f"Carpeta remota en S3: s3://{bucket_name}/{carpeta_remota}/")
+    except Exception as e:
+        print(f"Error subiendo {arch_local}: {e}")
 
-##### En esta parte el script va a crear un instancia EC2, y mover los archivos de la APP a sus directorios correspondientes
+print("")
+print(f"Archivos subidos al bucket: {bucket_name}")
+print("")
 
-import boto3
-import time
+######################################################################################################################################
+# 2: CREACION DE INSTANCIA EC2 Y EJECUCION DE COMANDOS DENTRO DE LA INSTANCIA (INSTALACION DE PAQUETES, MOVER ARCHIVOS, ETC)
+######################################################################################################################################
 
 ec2 = boto3.client('ec2')
 ssm = boto3.client('ssm')
 
-# Crear una instancia EC2 asociada al Instance Profile del rol LabRole
+# Parte 1: Crear una instancia EC2 asociada al Instance Profile del rol LabRole
 response = ec2.run_instances(
     ImageId='ami-06b21ccaeff8cd686',
+    InstanceType='t2.micro',
     MinCount=1,
     MaxCount=1,
-    InstanceType='t2.micro',
     IamInstanceProfile={'Name': 'LabInstanceProfile'},
 )
-instance_id = response['Instances'][0]['InstanceId']
-print(f"Instancia creada con ID: {instance_id}")
 
-# Esperar a que la instancia esté en estado running
+# Agregar tag Name: webserver-devops
+instance_id = response['Instances'][0]['InstanceId']
+ec2.create_tags(
+    Resources=[instance_id],
+    Tags=[{'Key': 'Name', 'Value': 'webserver-devops'}]
+)
+print(f"Instancia creada con ID: {instance_id} y tag 'webserver-devops'")
+print("")
+# Esperar a que la instancia esté en estado running 
 ec2.get_waiter('instance_status_ok').wait(InstanceIds=[instance_id])
 
 # Parte 2: Enviar comandos y extraer resultados
@@ -85,13 +85,10 @@ sudo systemctl start httpd
 sudo mkdir -p /var/www/html
 sudo mkdir -p /var/www
 
-# Copiar DIRECTAMENTE desde S3 al webroot
-sudo aws s3 cp s3://{bucket_name}/{carpeta_remota}/ /var/www/html/ --recursive
+# Copiar DIRECTAMENTE desde S3 los archivos hacia /var/www/html/ menos el archivo de la DB el cual lo copiamos despues a /var/www/
+sudo aws s3 sync s3://{bucket_name}/{carpeta_remota}/ /var/www/html/ --exclude "init_db.sql"
 
-# Mover init_db.sql FUERA del webroot
-if [ -f /var/www/html/init_db.sql ]; then
-    sudo mv /var/www/html/init_db.sql /var/www/
-fi
+sudo aws s3 cp s3://{bucket_name}/{carpeta_remota}/init_db.sql /var/www/
 
 # Ajustar permisos
 sudo chown -R apache:apache /var/www
@@ -108,9 +105,15 @@ command_id = response['Command']['CommandId']
 
 # Esperar resultado
 while True:
-    output = ssm.get_command_invocation(CommandId=command_id, InstanceId=instance_id)
-    if output['Status'] in ['Success', 'Failed', 'Cancelled', 'TimedOut']:
-        break
-    time.sleep(2)
-print("Output:")
+    try:
+        output = ssm.get_command_invocation(CommandId=command_id, InstanceId=instance_id)
+        if output['Status'] in ['Success', 'Failed', 'Cancelled', 'TimedOut']:
+            break
+        time.sleep(2)
+    except ssm.exceptions.InvocationDoesNotExist:
+        # Se agrega esto para evitar errores cuando el comando aún no fue recibido por el agente SSM
+        pass
+    time.sleep(3)
+print("EJECUCION DE COMANDOS E INSTALACION DE PAQUETES:")
+print("")
 print(output['StandardOutputContent'])
